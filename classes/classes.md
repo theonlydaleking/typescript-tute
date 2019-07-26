b3ba1b59d116348329f645195b2e53e7579165c7
Dale King <dale@studiomatrix.com.au>
2019-07-26 20:39:48 +1000

    started on classes

---
+++ classes/classes.md
@@ -0,0 +1,39 @@
+# Classes
+
+Blueprint to Create an object with some fields (values) and methods (functions) to represent a 'thing'
+
+They basically operate the same as they always have 
+
+
+## Methods 
+
+Just looks like es6 really: 
+
+```ts
+class Vehicle {
+  drive(): void {
+    console.log('Vrooooom')
+  }
+
+  honk(): void {
+    console.log('BEEEP')
+  }
+}
+
+const vehicle = new Vehicle()
+vehicle.drive()
+vehicle.honk()
+```
+
+
+### Public vs Private vs Protected
+
+| type      | result                                                      |
+| --------- | ----------------------------------------------------------- |
+| Public    | can be called anywhere any time.                            |
+| Private   | can only be called by other methods in _this_ class         |
+| Protected | Can be called by other methods in this, or by child classes |
+
+
+You use Private to stop other developers from calling something that they shouldn't (something that will fuck up the class for example)
+It has nothing to do with security really. 
---
+++ classes/classes.ts
@@ -0,0 +1,71 @@
+// Always capitlaise the name of a class
+class Vehicle {
+  drive(): void {
+    console.log('Vrooooom')
+  }
+
+  honk(): void {
+    console.log('BEEEP')
+  }
+}
+
+const vehicle = new Vehicle()
+vehicle.drive()
+vehicle.honk()
+
+// All pretty standard stuff. Infact just the same as ES6 so far.
+
+// Inheritance
+
+class Car extends Vehicle {
+  // A car is a type of vehicle. so it should inherit some shit
+  // just like react btw.
+  // You don't actually need super apparently
+  // Also you can overwirte shit like this
+  drive(): void {
+    console.log('fuckin... merrrrrrrr')
+  }
+}
+
+const car = new Car()
+
+car.drive()
+
+// Private, vs Public vs Protected
+
+class CarTwo {}
+
+class PrivateCar extends CarTwo {
+  // This fails because car has a public drive()
+  // just remove it if you need, or change it to protected
+  private drive(): void {
+    console.log('Vrooooom')
+  }
+
+  startDriving(): void {
+    this.drive()
+  }
+}
+
+const privateCar = new PrivateCar()
+// privateCar.honk()
+// See this returns an error
+// but this will work
+privateCar.startDriving()
+
+class ColourfulVehicle {
+  constructor(colour: string) {
+    this.colour = colour
+  }
+
+  colour: string
+  // usually you initialise it immediately
+  // if you don't want to tho?
+  // you can just call it like:
+  // constructor(public colour: string)
+  // it usually happens this way, but this is verbose.
+}
+
+const colourfulVehicle = new ColourfulVehicle('red')
+
+console.log(colourfulVehicle.colour)
