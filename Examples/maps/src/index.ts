b1d08f8a194afad074f606e43a763fe676093497
Dale King <dale@studiomatrix.com.au>
2019-07-29 13:56:28 +1000

    wow the type defs are useful.

--- Examples/maps/src/index.ts
+++ Examples/maps/src/index.ts
@@ -6,3 +6,11 @@ const company = new Company()
 
 console.log(user)
 console.log(company)
+
+const map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 3,
+  center: {
+    lat: company.location.lat,
+    lng: company.location.lng
+  }
+})
