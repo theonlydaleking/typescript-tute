1513ffe726577396a57312d54df90164f96db65a
Dale King <dale@studiomatrix.com.au>
2019-07-29 13:35:47 +1000

    class definitions for a User and a Company

--- Examples/maps/src/User.ts
+++ Examples/maps/src/User.ts
@@ -1,9 +1,20 @@
 import faker from 'faker'
 
-class User {
+export class User {
+  // you define the object first
   name: string
   location: {
     lat: number
     lng: number
+  }
+
+  // then use the constructor to build it.
+  constructor() {
+    this.name = faker.name.firstName()
+    // this.location.lat = 12 this will fail because constructor doesn't get called until after initialisation.
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    }
   }
 }
