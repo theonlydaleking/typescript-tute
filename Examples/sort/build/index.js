591238c2c00d230ce39caee2e23028f1b8ee6193
Dale King <dale@studiomatrix.com.au>
2019-07-31 18:24:22 +1000

    bubble sort for numbers done

--- Examples/sort/build/index.js
+++ Examples/sort/build/index.js
@@ -5,7 +5,29 @@ var Sorter = /** @class */ (function () {
     function Sorter(collection) {
         this.collection = collection;
     }
-    Sorter.prototype.sort = function () { };
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                // don't worry about the algorithm, it's not important.
+                // but this is bubble sort
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                    // Yuck
+                }
+            }
+        }
+    };
     return Sorter;
 }());
 var sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection);
+// Now to get it work with strings
+// you cant do
+// const str = "hey there"
+// str[0] = Y
+// Strings are immutable so if you try log it:
+// console.log(str); // "hey there"
