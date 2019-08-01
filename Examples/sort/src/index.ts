4ea286dee48c5902ce7f7039a498f7fff43e9bcc
Dale King <dale@studiomatrix.com.au>
2019-08-01 18:11:24 +1000

    string sort done

--- Examples/sort/src/index.ts
+++ Examples/sort/src/index.ts
@@ -1,7 +1,14 @@
 import { WorkingSorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection'
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0])
 const sorter = new WorkingSorter(numbersCollection)
 sorter.sort()
 console.log(sorter.collection)
+
+const charactersColection = new CharactersCollection('heyThere')
+const stringSorter = new WorkingSorter(charactersColection)
+
+stringSorter.sort()
+console.log(stringSorter.collection)
