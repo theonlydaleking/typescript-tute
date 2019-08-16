1f8386cd02bb2e8561f96ab0a9ca44eb4b0dc481
Dale King <dale@studiomatrix.com.au>
2019-08-16 18:45:22 +1000

    moved to using csvFileReader class

--- Examples/stats/build/index.js
+++ Examples/stats/build/index.js
@@ -1,2 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var CsvFileReader_1 = require("./CsvFileReader");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+var manUnitedWins = 0;
+// This is called a "For - Of loop" from es6. Look it up
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log("Man united won " + manUnitedWins + " games");
--- Examples/stats/src/index.ts
+++ Examples/stats/src/index.ts
@@ -1,2 +1,24 @@
-import fs from 'fs'
+import { CsvFileReader } from './CsvFileReader'
 
+const reader = new CsvFileReader('football.csv')
+
+reader.read()
+
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+let manUnitedWins = 0
+
+// This is called a "For - Of loop" from es6. Look it up
+for (let match of reader.data) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++
+  }
+}
+
+console.log(`Man united won ${manUnitedWins} games`)
