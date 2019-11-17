05d7b18d305b825ca96184f3884960d851f459cf
Dale King <dale@studiomatrix.com.au>
2019-11-17 18:35:57 +1100

    Generic Constraints

--- features/Generics/generix.ts
+++ features/Generics/generix.ts
@@ -64,3 +64,31 @@ const printAnything = <T>(arr: T[]): void => {
 // However it is recomended you annotate it yourself.
 // This will stop other devs from accidentally passing in numbers
 printAnything<string>(["a", "b"]);
+
+// Example Generic Constraints
+
+class Car {
+  print() {
+    console.log("i am a car");
+  }
+}
+
+class House {
+  print() {
+    console.log("i am a House");
+  }
+}
+
+interface Printable {
+  print(): void;
+}
+
+// extends Printable makes sure that whatever you put in, has a print method attached
+const printHousesOrCars = <T extends Printable>(arr: T[]): void => {
+  arr.forEach(i => {
+    i.print();
+  });
+};
+printHousesOrCars([1, 2, 3]); // doesnt work
+
+printHousesOrCars([new House(), new Car()]); // works
